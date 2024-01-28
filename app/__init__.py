c59763f47b4d3460c8fa0401d664d4fc88c9c91e
Warren Whipple <w@warrenwhipple.com>
2024-01-28 17:15:51 +0000

    Basic openai connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,26 @@
 import os
 
+from openai import OpenAI
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 load_dotenv()
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+slack_app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+openai_client = OpenAI()
 
 
-@app.message("hello")
+@slack_app.message()
 def message_hello(message, say):
-    user = message["user"]
-    say(f"Hi there, <@{user}>!")
+    chat_completion = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message["text"]}],
+    )
+    say(chat_completion.choices[0].message.content)
 
 
 def main():
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    SocketModeHandler(slack_app, os.environ["SLACK_APP_TOKEN"]).start()
 
 
 if __name__ == "__main__":
